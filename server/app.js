606222bc64a57e57dd3ee6a4e159a031426d9b5f
dabeii <tranlan0310@gmail.com>
2022-12-23 12:06:59 +0700

    up

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ app.use(bodyP.urlencoded({ extended: false }));
 app.use(bodyP.json());
 
 app.use(cors({
-    origin: ['https://https://herokunodejsg2.herokuapp.com/', 'http://localhost:8080']
+    origin: ['http://https://herokunodejsg2.herokuapp.com/']
 }))
 
 mongoose.set("strictQuery", false);
